546f66af4073a64915365137babff659a93763b1
irawanyulianto <irawanyulianto77@gmail.com>
2023-12-20 10:54:17 +0700

    Update velocity-quiz.php

--- velocity-quiz.php
+++ velocity-quiz.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://velocitydeveloper.com
+ * @since             1.0.0
+ * @package           Velocity_Quiz
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Velocity Quiz
+ * Plugin URI:        https://velocitydeveloper.com
+ * Description:       Quiz plugin for Velocitydeveloper Client
+ * Version:           1.0.0.0
+ * Author:            Velocity
+ * Author URI:        https://velocitydeveloper.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       velocity-quiz
+ * Domain Path:       /languages
+ */
