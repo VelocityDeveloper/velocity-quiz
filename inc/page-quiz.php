77e716a4ddb9d2dfd50d90783a0151a8ce3213f6
irawanyulianto <irawanyulianto77@gmail.com>
2023-12-22 15:30:34 +0700

    s

--- inc/page-quiz.php
+++ inc/page-quiz.php
@@ -22,21 +22,78 @@
 </nav>
 
 
-<?php if (isset($_POST['quiz'])) {
-  echo '<pre>'.print_r($_POST,1).'</pre>';
+<?php
+$post_title         = isset($_POST['post_title']) ? $_POST['post_title'] : '';
+$post_content       = isset($_POST['post_content']) ? $_POST['post_content'] : '';
+$post_categories    = isset($_POST['quizcat']) ? $_POST['quizcat'] : '';
+$quiz               = isset($_POST['quiz']) ? $_POST['quiz'] : '';
+$status             = isset($_POST['status']) ? $_POST['status'] : '';
+$waktu              = isset($_POST['waktu']) ? $_POST['waktu'] : '';
+$tampil_nilai       = isset($_POST['tampil_nilai']) ? $_POST['tampil_nilai'] : '';
+$safe               = isset($_POST['safe']) ? $_POST['safe'] : '';
+
+// $all_meta_values = get_post_meta(49,'quiz',false);
+// echo '<pre>'.print_r($all_meta_values,1).'</pre>';
+
+if ($quiz) {
+  // Loop melalui sub-array 'tanya'
+  foreach ($quiz['tanya'] as $key => $value) {
+    // Membuat array baru dengan struktur yang diinginkan
+    $new_quiz_array[] = array(
+        'tanya' => $value,
+        'a' => $quiz['a'][$key],
+        'b' => $quiz['b'][$key],
+        'c' => $quiz['c'][$key],
+        'd' => $quiz['d'][$key],
+        'jawaban' => $quiz['jawaban'][$key]
+    );
+  }
+
+  // Mengganti sub-array 'quiz' dengan array yang baru dibuat
+  $quiz = $new_quiz_array;
+
+  // Output hasil transformasi
+  echo '<pre>'.print_r($quiz,1).'</pre>';
 }
-$val = '';
+
+
+
+/*
+if ($post_title && $quiz) {
+  $quiz_post = array(
+    'post_title'    => $post_title,
+    'post_content'  => $post_content,
+    'post_status'   => $status,
+    'post_type'     => 'velocity-quiz',
+  );
+  $pid = wp_insert_post($quiz_post,true);
+  if(!is_wp_error($pid)) {
+    if($post_categories){
+      wp_set_object_terms($pid,$post_categories,'quiz-category');
+    }
+    update_post_meta($pid,'waktu',$waktu);
+    update_post_meta($pid,'tampil_nilai',$tampil_nilai);
+    update_post_meta($pid,'safe',$safe);
+    update_post_meta($pid,'quiz',$quiz);
+    echo '<div class="alert alert-success">Quiz berhasil disimpan.</div>';
+  } else {
+    echo '<div class="alert alert-danger">Quiz gagal disimpan.</div>';
+  }
+}
+*/
 ?>
 
 <form method="post">
   <div class="velocity-field">
-    <h4 class="fs-5 fw-bold">Informasi Quiz</h5>
+
+  <?php /* ?>
     <div class="border p-3 mb-3">
-      <h5 class="vd-field-title mt-0">Judul</h5>
+      <h5 class="vd-field-title mt-0">Judul Quiz</h5>
       <input type="text" class="form-control" name="post_title" required>
 
       <h5 class="vd-field-title">Keterangan</h5>
-      <?php wp_editor($val,'post_content'); ?>
+      <?php $val = '';
+      wp_editor($val,'post_content'); ?>
 
       <?php 
       $tax_args = array(
@@ -45,24 +102,40 @@ $val = '';
       );
       $terms = get_terms($tax_args); ?>
       <h5 class="vd-field-title">Kategori</h5>
-      <select class="form-select" name="category">
+      <select class="form-select" name="quizcat">
         <option value="">Pilih Kategori</option>
         <?php foreach ($terms as $term) {
           echo '<option value="' . $term->slug . '">' . $term->name . '</option>';
         } ?>
       </select>
+
+      <h5 class="vd-field-title">Waktu Pengerjaan</h5>
+      <input type="number" class="form-control" name="waktu">
+      <small class="text-muted">Dalam menit. Kosongkan jika tanpa batas waktu pengerjaan.</small>
+
+      <h5 class="vd-field-title">Tampil Nilai</h5>
+      <select class="form-select" name="tampil_nilai">
+        <option value="Ya">Ya</option>
+        <option value="Tidak">Tidak</option>
+      </select>
+      <small class="text-muted">Jika aktif, hasil nilai akan langsung keluar.</small>
+
+      <h5 class="vd-field-title">Keamanan</h5>
+      <select class="form-select" name="safe">
+        <option value="Tidak">Tidak</option>
+        <option value="Ya">Ya</option>
+      </select>
+      <small class="text-muted">Jika aktif, ujian dianggap selesai jika meninggalkan tabs atau browser ketika ujian berlangsung.</small>
+
     </div>
+  <?php */ ?>
+
 
     <h4 class="fs-5 fw-bold mt-4">Pertanyaan</h5>
     <div class="velocity-form-control">
-      <h5 class="vd-field-title mt-0">Gambar soal</h5>
-          <div class="vd-upload-file"><a style="cursor:pointer" class="btn btn-secondary btn-sm text-white" onClick="open_media_uploader_image('imagesoal');">Pilih Gambar</a>
-          <div class="imagesoal" id="imagesoal"></div>
-          <input type="hidden" name="quiz[imgsoal][]" id="idimagesoal" value="" readonly/>
-        </div>
 
-      <h5 class="vd-field-title">Pertanyaan</h5>
-        <textarea class="form-control" id="ask" name="quiz[tanya][]"></textarea>
+      <h5 class="vd-field-title mt-0">Soal</h5>
+      <textarea class="form-control" id="ask" name="quiz[tanya][]"></textarea>
 
     <?php $pilihan_jawaban = '<h5 class="vd-field-title">Pilihan Jawaban</h5>';
         $pilihan_jawaban .= '<div class="input-group mb-2">';
@@ -97,41 +170,20 @@ $val = '';
         ?>
     </div>
   </div>
-  <div type="button" id="tambah" class="btn btn-sm btn-info text-white">Tambah Soal</div>
-  <button type="submit" class="btn btn-sm btn-success">Simpan</button>
+  <h5 class="vd-field-title mt-3">Pilih Status</h5>
+  <select class="form-select mb-4" name="status" required="">
+    <option value="publish">Publish</option>
+    <option value="draft">Draft</option>
+  </select>
+
+  <div id="tambah" class="btn btn-info text-white">Tambah Soal</div>
+  <button type="submit" class="btn btn-success">Simpan</button>
 </form>
 
 <?php echo wp_enqueue_editor(); ?>
 <?php echo wp_enqueue_media(); ?>
 
 <script>
-  var media_uploader = null;
-  function open_media_uploader_image(location_id) {
-      media_uploader = wp.media({
-          frame: "post",
-          state: "insert",
-          library: {
-            type: ['image']
-          },
-          multiple: false
-      });
-      media_uploader.on("insert", function(){
-        var json = media_uploader.state().get("selection").first().toJSON();
-        var image_url = json.url;
-        var image_id = json.id;
-        document.getElementById(location_id).innerHTML = '<span class="hapusimgsoal">x</span><img src="'+image_url+'" class="m-1">';
-        document.getElementById('id'+location_id).value = image_id;
-      });
-      media_uploader.open();
-  }
-	jQuery(document).ready(function($){
-		$(document).on("click",".hapusimgsoal",function(e){
-      $(this).parent().html('');
-      var id = $(this).parent().prop('className');
-      $('#id'+id).val('');
-		});
-	});
-
   function hapus(id) {
     if (confirm('Hapus ini?')){
       document.getElementById(id).remove();
@@ -151,7 +203,7 @@ jQuery(function($) {
           toolbar2: 'formatselect alignjustify forecolor | pastetext removeformat charmap | outdent indent | undo redo | wp_help'
         },
         quicktags: true,
-        //mediaButtons: true,
+        mediaButtons: true,
       }
     );
   });
@@ -160,15 +212,13 @@ jQuery(function($) {
   $("#tambah").click(function(){
     i++;
     var function_hapus = "hapus('velocity-field-"+i+"');";
-    var function_media = "open_media_uploader_image('imagesoal"+i+"');";
     var awal = '<div class="velocity-form-control" id="velocity-field-'+i+'">';
     var close = '<div class="vd-hapus" onClick="'+function_hapus+'">x</div>';
-    var gambar = '<h5 class="vd-field-title mt-3">Gambar soal</h5><div class="vd-upload-file"><a style="cursor:pointer" class="btn btn-secondary btn-sm text-white" onClick="'+function_media+'">Pilih Gambar</a></div><div class="imagesoal" id="imagesoal'+i+'"></div><input type="hidden" name="quiz[imgsoal][]" id="idimagesoal'+i+'" value="" readonly/>';
-    var ask = '<h5 class="vd-field-title">Pertanyaan</h5><textarea class="form-control" id="ask'+i+'" name="quiz[tanya][]"></textarea>';
+    var ask = '<h5 class="vd-field-title">Soal</h5><textarea class="form-control" id="ask'+i+'" name="quiz[tanya][]"></textarea>';
     var pj = '<?php echo $pilihan_jawaban;?>';
     var jwb = '<?php echo $jawaban;?>';
     var akhir = '</div>';
-    $(".velocity-field").append(awal+close+gambar+ask+pj+jwb+akhir);
+    $(".velocity-field").append(awal+close+ask+pj+jwb+akhir);
     wp.editor.initialize(
       'ask'+i,
       {
@@ -179,6 +229,7 @@ jQuery(function($) {
           toolbar2: 'formatselect alignjustify forecolor | pastetext removeformat charmap | outdent indent | undo redo | wp_help'
         },
         quicktags: true,
+        mediaButtons: true,
       }
     );
   });
